558f9341290280fa12c4d2e3f79fccb27842e0f0
bitsky <bitdust16@gmail.com>
2014-06-18 23:30:06 +0800

    Add ping-pong buffer mode - add the parameter '--pingpong' to output data to ping.dat and pong.dat alternately.

--- host/examples/rx_samples_to_file.cpp
+++ host/examples/rx_samples_to_file.cpp
@@ -45,43 +45,55 @@ template<typename samp_type> void recv_to_file(
     bool stats = false,
     bool null = false,
     bool enable_size_map = false,
-    bool continue_on_bad_packet = false
-){
-    unsigned long long num_total_samps = 0;
-    //create a receive streamer
-    uhd::stream_args_t stream_args(cpu_format,wire_format);
-    uhd::rx_streamer::sptr rx_stream = usrp->get_rx_stream(stream_args);
+    bool continue_on_bad_packet = false,
+    bool pingpong = false,
+    unsigned long long pingpong_size = 5000000
+    ){
+        unsigned long long num_total_samps = 0;
+        unsigned long long cnt = 0;
+        //create a receive streamer
+        uhd::stream_args_t stream_args(cpu_format,wire_format);
+        uhd::rx_streamer::sptr rx_stream = usrp->get_rx_stream(stream_args);
 
-    uhd::rx_metadata_t md;
-    std::vector<samp_type> buff(samps_per_buff);
-    std::ofstream outfile;
-    if (not null)
-		outfile.open(file.c_str(), std::ofstream::binary);
-    bool overflow_message = true;
+        uhd::rx_metadata_t md;
+        std::vector<samp_type> buff(samps_per_buff);
+        std::ofstream outfile;
+        bool isfileA;
+        if (not null) {
+            if (not pingpong)
+                outfile.open(file.c_str(), std::ofstream::binary);
+            else {
+                outfile.open("ping.dat", std::ofstream::binary);
+                isfileA = true;
+            }
 
-    //setup streaming
-    uhd::stream_cmd_t stream_cmd((num_requested_samples == 0)?
-        uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS:
+        }
+        bool overflow_message = true;
+
+        //setup streaming
+        uhd::stream_cmd_t stream_cmd((num_requested_samples == 0)?
+            uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS:
         uhd::stream_cmd_t::STREAM_MODE_NUM_SAMPS_AND_DONE
-    );
-    stream_cmd.num_samps = num_requested_samples;
-    stream_cmd.stream_now = true;
-    stream_cmd.time_spec = uhd::time_spec_t();
-    rx_stream->issue_stream_cmd(stream_cmd);
-    
-    boost::system_time start = boost::get_system_time();
-    unsigned long long ticks_requested = (long)(time_requested * (double)boost::posix_time::time_duration::ticks_per_second());
-    boost::posix_time::time_duration ticks_diff;
-    boost::system_time last_update = start;
-    unsigned long long last_update_samps = 0;
-    
-    typedef std::map<size_t,size_t> SizeMap;
-    SizeMap mapSizes;
+            );
+        stream_cmd.num_samps = num_requested_samples;
+        stream_cmd.stream_now = true;
+        stream_cmd.time_spec = uhd::time_spec_t();
+        rx_stream->issue_stream_cmd(stream_cmd);
 
-    while(not stop_signal_called and (num_requested_samples != num_total_samps or num_requested_samples == 0)){
-		boost::system_time now = boost::get_system_time();
-		
-        size_t num_rx_samps = rx_stream->recv(&buff.front(), buff.size(), md, 3.0, enable_size_map);
+        boost::system_time start = boost::get_system_time();
+        unsigned long long ticks_requested = (long)(time_requested * (double)boost::posix_time::time_duration::ticks_per_second());
+        boost::posix_time::time_duration ticks_diff;
+        boost::system_time last_update = start;
+        unsigned long long last_update_samps = 0;
+        
+
+        typedef std::map<size_t,size_t> SizeMap;
+        SizeMap mapSizes;
+
+        while(not stop_signal_called and (num_requested_samples != num_total_samps or num_requested_samples == 0)){
+            boost::system_time now = boost::get_system_time();
+
+            size_t num_rx_samps = rx_stream->recv(&buff.front(), buff.size(), md, 3.0, enable_size_map);
 
         if (md.error_code == uhd::rx_metadata_t::ERROR_CODE_TIMEOUT) {
             std::cout << boost::format("Timeout while streaming") << std::endl;
@@ -110,101 +122,125 @@ template<typename samp_type> void recv_to_file(
                 throw std::runtime_error(error);
         }
 
-        if (enable_size_map){
-			SizeMap::iterator it = mapSizes.find(num_rx_samps);
-			if (it == mapSizes.end())
-				mapSizes[num_rx_samps] = 0;
-			mapSizes[num_rx_samps] += 1;
-		}
+            if (enable_size_map){
+                SizeMap::iterator it = mapSizes.find(num_rx_samps);
+                if (it == mapSizes.end())
+                    mapSizes[num_rx_samps] = 0;
+                mapSizes[num_rx_samps] += 1;
+            }
 
-        num_total_samps += num_rx_samps;
+            num_total_samps += num_rx_samps;
+            cnt += num_rx_samps;
+            if (pingpong)
+            {
+                if (cnt >= pingpong_size)
+                {
+                    //std::cout << "tick!"<<std::endl;
+                    if (isfileA)
+                    {
+                        if (outfile.is_open())
+                            outfile.close();
+                        outfile.open("ping.dat", std::ofstream::binary);
+                        isfileA = false;
+                        //std::cout << "pingpongB.dat"<<std::endl;
+                    } 
+                    else
+                    {
+                        if (outfile.is_open())
+                            outfile.close();
+                        outfile.open("pong.dat", std::ofstream::binary);
+                        //std::cout << "pingpongA.dat"<<std::endl;
+                        isfileA = true;
+                    }
+                    cnt = 0;
+                }
+            }
+            if (outfile.is_open())
+                outfile.write((const char*)&buff.front(), num_rx_samps*sizeof(samp_type));
 
-		if (outfile.is_open())
-			outfile.write((const char*)&buff.front(), num_rx_samps*sizeof(samp_type));
+            if (bw_summary){
+                last_update_samps += num_rx_samps;
+                boost::posix_time::time_duration update_diff = now - last_update;
+                if (update_diff.ticks() > boost::posix_time::time_duration::ticks_per_second()) {
+                    double t = (double)update_diff.ticks() / (double)boost::posix_time::time_duration::ticks_per_second();
+                    double r = (double)last_update_samps / t;
+                    std::cout << boost::format("\t%f Msps") % (r/1e6) << std::endl;
+                    last_update_samps = 0;
+                    last_update = now;
+                }
+            }
 
-		if (bw_summary){
-			last_update_samps += num_rx_samps;
-			boost::posix_time::time_duration update_diff = now - last_update;
-			if (update_diff.ticks() > boost::posix_time::time_duration::ticks_per_second()) {
-				double t = (double)update_diff.ticks() / (double)boost::posix_time::time_duration::ticks_per_second();
-				double r = (double)last_update_samps / t;
-				std::cout << boost::format("\t%f Msps") % (r/1e6) << std::endl;
-				last_update_samps = 0;
-				last_update = now;
-			}
-		}
-        
-        ticks_diff = now - start;
-		if (ticks_requested > 0){
-			if ((unsigned long long)ticks_diff.ticks() > ticks_requested)
-				break;
-		}
-    }
-    
-    if (outfile.is_open())
-		outfile.close();
-    
-    if (stats){
-		std::cout << std::endl;
+            ticks_diff = now - start;
+            if (ticks_requested > 0){
+                if ((unsigned long long)ticks_diff.ticks() > ticks_requested)
+                    break;
+            }
+        }
 
-		double t = (double)ticks_diff.ticks() / (double)boost::posix_time::time_duration::ticks_per_second();
-		std::cout << boost::format("Received %d samples in %f seconds") % num_total_samps % t << std::endl;
-		double r = (double)num_total_samps / t;
-		std::cout << boost::format("%f Msps") % (r/1e6) << std::endl;
-		
-		if (enable_size_map) {
-			std::cout << std::endl;
-			std::cout << "Packet size map (bytes: count)" << std::endl;
-			for (SizeMap::iterator it = mapSizes.begin(); it != mapSizes.end(); it++)
-				std::cout << it->first << ":\t" << it->second << std::endl;
-		}
-	}
+        if (outfile.is_open())
+            outfile.close();
+
+        if (stats){
+            std::cout << std::endl;
+
+            double t = (double)ticks_diff.ticks() / (double)boost::posix_time::time_duration::ticks_per_second();
+            std::cout << boost::format("Received %d samples in %f seconds") % num_total_samps % t << std::endl;
+            double r = (double)num_total_samps / t;
+            std::cout << boost::format("%f Msps") % (r/1e6) << std::endl;
+
+            if (enable_size_map) {
+                std::cout << std::endl;
+                std::cout << "Packet size map (bytes: count)" << std::endl;
+                for (SizeMap::iterator it = mapSizes.begin(); it != mapSizes.end(); it++)
+                    std::cout << it->first << ":\t" << it->second << std::endl;
+            }
+        }
 }
 
 typedef boost::function<uhd::sensor_value_t (const std::string&)> get_sensor_fn_t;
 
 bool check_locked_sensor(std::vector<std::string> sensor_names, const char* sensor_name, get_sensor_fn_t get_sensor_fn, double setup_time){
-	if (std::find(sensor_names.begin(), sensor_names.end(), sensor_name) == sensor_names.end())
-		return false;
-	
-	boost::system_time start = boost::get_system_time();
-	boost::system_time first_lock_time;
-	
-	std::cout << boost::format("Waiting for \"%s\": ") % sensor_name;
-	std::cout.flush();
-	
-	while (true){
-		if ((not first_lock_time.is_not_a_date_time()) and
-			(boost::get_system_time() > (first_lock_time + boost::posix_time::seconds(setup_time))))
-		{
-			std::cout << " locked." << std::endl;
-			break;
-		}
-		
-		if (get_sensor_fn(sensor_name).to_bool()){
-			if (first_lock_time.is_not_a_date_time())
-				first_lock_time = boost::get_system_time();
-			std::cout << "+";
-			std::cout.flush();
-		}
-		else{
-			first_lock_time = boost::system_time();	//reset to 'not a date time'
-			
-			if (boost::get_system_time() > (start + boost::posix_time::seconds(setup_time))){
-				std::cout << std::endl;
-				throw std::runtime_error(str(boost::format("timed out waiting for consecutive locks on sensor \"%s\"") % sensor_name));
-			}
-			
-			std::cout << "_";
-			std::cout.flush();
-		}
-		
-		boost::this_thread::sleep(boost::posix_time::milliseconds(100));
-	}
-	
-	std::cout << std::endl;
-	
-	return true;
+    if (std::find(sensor_names.begin(), sensor_names.end(), sensor_name) == sensor_names.end())
+        return false;
+
+    boost::system_time start = boost::get_system_time();
+    boost::system_time first_lock_time;
+
+    std::cout << boost::format("Waiting for \"%s\": ") % sensor_name;
+    std::cout.flush();
+
+    while (true){
+        if ((not first_lock_time.is_not_a_date_time()) and
+            (boost::get_system_time() > (first_lock_time + boost::posix_time::seconds(setup_time))))
+        {
+            std::cout << " locked." << std::endl;
+            break;
+        }
+
+        if (get_sensor_fn(sensor_name).to_bool()){
+            if (first_lock_time.is_not_a_date_time())
+                first_lock_time = boost::get_system_time();
+            std::cout << "+";
+            std::cout.flush();
+        }
+        else{
+            first_lock_time = boost::system_time(); //reset to 'not a date time'
+
+            if (boost::get_system_time() > (start + boost::posix_time::seconds(setup_time))){
+                std::cout << std::endl;
+                throw std::runtime_error(str(boost::format("timed out waiting for consecutive locks on sensor \"%s\"") % sensor_name));
+            }
+
+            std::cout << "_";
+            std::cout.flush();
+        }
+
+        boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+    }
+
+    std::cout << std::endl;
+
+    return true;
 }
 
 int UHD_SAFE_MAIN(int argc, char *argv[]){
@@ -212,7 +248,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
 
     //variables to be set by po
     std::string args, file, type, ant, subdev, ref, wirefmt;
-    size_t total_num_samps, spb;
+    size_t total_num_samps, spb, pingpong_size;
     double rate, freq, gain, bw, total_time, setup_time;
 
     //setup the program options
@@ -241,7 +277,9 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
         ("continue", "don't abort on a bad packet")
         ("skip-lo", "skip checking LO lock status")
         ("int-n", "tune USRP with integer-N tuning")
-    ;
+        ("pingpong","output the data into ping.dat and pong.dat as ping-pong buffer")
+        ("pingpong_size",po::value<size_t>(&pingpong_size)->default_value(5000000), "size of each ping-pong file")
+        ;
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
@@ -251,15 +289,16 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
         std::cout << boost::format("UHD RX samples to file %s") % desc << std::endl;
         return ~0;
     }
-    
+
     bool bw_summary = vm.count("progress") > 0;
     bool stats = vm.count("stats") > 0;
     bool null = vm.count("null") > 0;
     bool enable_size_map = vm.count("sizemap") > 0;
     bool continue_on_bad_packet = vm.count("continue") > 0;
-    
+    bool pingpong = vm.count("pingpong") > 0;
+
     if (enable_size_map)
-		std::cout << "Packet size tracking enabled - will only recv one packet at a time!" << std::endl;
+        std::cout << "Packet size tracking enabled - will only recv one packet at a time!" << std::endl;
 
     //create a usrp device
     std::cout << std::endl;
@@ -284,13 +323,13 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
     std::cout << boost::format("Actual RX Rate: %f Msps...") % (usrp->get_rx_rate()/1e6) << std::endl << std::endl;
 
     //set the center frequency
-    if (vm.count("freq")){	//with default of 0.0 this will always be true
-		std::cout << boost::format("Setting RX Freq: %f MHz...") % (freq/1e6) << std::endl;
+    if (vm.count("freq")){  //with default of 0.0 this will always be true
+        std::cout << boost::format("Setting RX Freq: %f MHz...") % (freq/1e6) << std::endl;
         uhd::tune_request_t tune_request(freq);
         if(vm.count("int-n")) tune_request.args = uhd::device_addr_t("mode_n=integer");
-		usrp->set_rx_freq(tune_request);
-		std::cout << boost::format("Actual RX Freq: %f MHz...") % (usrp->get_rx_freq()/1e6) << std::endl << std::endl;
-	}
+        usrp->set_rx_freq(tune_request);
+        std::cout << boost::format("Actual RX Freq: %f MHz...") % (usrp->get_rx_freq()/1e6) << std::endl << std::endl;
+    }
 
     //set the rf gain
     if (vm.count("gain")){
@@ -313,12 +352,12 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
 
     //check Ref and LO Lock detect
     if (not vm.count("skip-lo")){
-		check_locked_sensor(usrp->get_rx_sensor_names(0), "lo_locked", boost::bind(&uhd::usrp::multi_usrp::get_rx_sensor, usrp, _1, 0), setup_time);
-		if (ref == "mimo")
-			check_locked_sensor(usrp->get_mboard_sensor_names(0), "mimo_locked", boost::bind(&uhd::usrp::multi_usrp::get_mboard_sensor, usrp, _1, 0), setup_time);
-		if (ref == "external")
-			check_locked_sensor(usrp->get_mboard_sensor_names(0), "ref_locked", boost::bind(&uhd::usrp::multi_usrp::get_mboard_sensor, usrp, _1, 0), setup_time);
-	}
+        check_locked_sensor(usrp->get_rx_sensor_names(0), "lo_locked", boost::bind(&uhd::usrp::multi_usrp::get_rx_sensor, usrp, _1, 0), setup_time);
+        if (ref == "mimo")
+            check_locked_sensor(usrp->get_mboard_sensor_names(0), "mimo_locked", boost::bind(&uhd::usrp::multi_usrp::get_mboard_sensor, usrp, _1, 0), setup_time);
+        if (ref == "external")
+            check_locked_sensor(usrp->get_mboard_sensor_names(0), "ref_locked", boost::bind(&uhd::usrp::multi_usrp::get_mboard_sensor, usrp, _1, 0), setup_time);
+    }
 
     if (total_num_samps == 0){
         std::signal(SIGINT, &sig_int_handler);
@@ -326,7 +365,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
     }
 
 #define recv_to_file_args(format) \
-	(usrp, format, wirefmt, file, spb, total_num_samps, total_time, bw_summary, stats, null, enable_size_map, continue_on_bad_packet)
+    (usrp, format, wirefmt, file, spb, total_num_samps, total_time, bw_summary, stats, null, enable_size_map, continue_on_bad_packet,pingpong,pingpong_size)
     //recv to file
     if (type == "double") recv_to_file<std::complex<double> >recv_to_file_args("fc64");
     else if (type == "float") recv_to_file<std::complex<float> >recv_to_file_args("fc32");
